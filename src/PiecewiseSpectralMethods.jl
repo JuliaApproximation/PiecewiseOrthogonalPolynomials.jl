1061c571b8b0673e004ce2c84b7296a11de346f4
Sheehan Olver <solver@mac.com>
2022-04-01 19:52:44 +0100

    Transform

--- src/PiecewiseSpectralMethods.jl
+++ src/PiecewiseSpectralMethods.jl
@@ -1,11 +1,11 @@
 module PiecewiseSpectralMethods
 using ClassicalOrthogonalPolynomials, LinearAlgebra, BlockArrays, BlockBandedMatrices, ContinuumArrays, QuasiArrays, LazyArrays, LazyBandedMatrices, FillArrays
 
-import BlockArrays: BlockSlice, block, blockindex
+import BlockArrays: BlockSlice, block, blockindex, blockvec
 import BlockBandedMatrices: _BandedBlockBandedMatrix
 import ClassicalOrthogonalPolynomials: grid, massmatrix
-import ContinuumArrays: @simplify
-import Base: axes, getindex, ==, \
+import ContinuumArrays: @simplify, factorize, TransformFactorization
+import Base: axes, getindex, ==, \, OneTo
 
 export PiecewisePolynomial, ContinuousPolynomial
 
@@ -21,6 +21,41 @@ PiecewisePolynomial{T}(basis::AbstractQuasiMatrix, points::AbstractVector) where
 PiecewisePolynomial(basis::AbstractQuasiMatrix{T}, points::AbstractVector) where {T} =
     PiecewisePolynomial{T}(basis, points)
 
+axes(B::PiecewisePolynomial) = (Inclusion(first(B.points) .. last(B.points)), blockedrange(Fill(length(B.points) - 1, ∞)))
+
+function repeatgrid(ax, g, pts)
+    ret = Matrix{eltype(g)}(undef, length(g), length(pts) - 1)
+    for j in axes(ret, 2)
+        ret[:, j] = affine(ax, pts[j] .. pts[j+1])[g]
+    end
+    ret
+end
+
+function grid(V::SubQuasiArray{T,2,<:PiecewisePolynomial,<:Tuple{Inclusion,BlockSlice}}) where {T}
+    P = parent(V)
+    _, JR = parentindices(V)
+    N = Int(last(JR))
+    g = grid(P.basis[:, OneTo(N)])
+    repeatgrid(axes(P.basis, 1), g, P.points)
+end
+
+struct ApplyFactorization{T, FF, FAC<:Factorization{T}} <: Factorization{T}
+    f::FF
+    F::FAC
+end
+
+\(P::ApplyFactorization, f) = P.f(P.F \ f)
+
+
+function factorize(V::SubQuasiArray{<:Any,2,<:PiecewisePolynomial,<:Tuple{Inclusion,BlockSlice}})
+    P = parent(V)
+    _,JR = parentindices(V)
+    N = Int(last(JR.block))
+    F = factorize(view(P.basis,:,OneTo(N)), length(P.points)-1)
+    ApplyFactorization(v -> blockvec(permutedims(v)), TransformFactorization(repeatgrid(axes(P.basis, 1), F.grid, P.points), F.plan))
+end
+
+
 struct ContinuousPolynomial{order,T,P<:AbstractVector} <: AbstractPiecewisePolynomial{order,T,P}
     points::P
 end
@@ -31,9 +66,6 @@ ContinuousPolynomial{o}(pts) where {o} = ContinuousPolynomial{o,Float64}(pts)
 ContinuousPolynomial{o}(P::ContinuousPolynomial) where {o} = ContinuousPolynomial{o,eltype(P)}(P.points)
 
 PiecewisePolynomial(P::ContinuousPolynomial{0,T}) where {T} = PiecewisePolynomial(Legendre{T}(), P.points)
-
-axes(B::PiecewisePolynomial) =
-    (Inclusion(first(B.points) .. last(B.points)), blockedrange(Fill(length(B.points) - 1, ∞)))
 
 axes(B::ContinuousPolynomial{0}) where {o} = axes(PiecewisePolynomial(B))
 axes(B::ContinuousPolynomial{1}) where {o} =
@@ -54,7 +86,7 @@ function getindex(P::PiecewisePolynomial{T}, x::Number, Kk::BlockIndex{1}) where
         zero(T)
     end
 end
-getindex(P::ContinuousPolynomial{0,T}, x::Number, Kk::BlockIndex{1}) where {T} = PiecewisePolynomial(P)
+getindex(P::ContinuousPolynomial{0,T}, x::Number, Kk::BlockIndex{1}) where {T} = PiecewisePolynomial(P)[x, Kk]
 
 function getindex(P::ContinuousPolynomial{1,T}, x::Number, Kk::BlockIndex{1}) where {T}
     K, k = block(Kk), blockindex(Kk)
@@ -73,19 +105,8 @@ end
 
 getindex(P::AbstractPiecewisePolynomial, x::Number, k::Int) = P[x, findblockindex(axes(P, 2), k)]
 
-
-function grid(V::SubQuasiArray{T,2,<:PiecewisePolynomial,<:Tuple{Inclusion,BlockSlice}}) where {T}
-    P = parent(V)
-    _, JR = parentindices(Q)
-    N = Int(last(JR))
-    g = grid(P[:, OneTo(N)])
-    pts = P.points
-    ret = Matrix{T}(undef, length(g), length(pts) - 1)
-    for j in axes(ret, 2)
-        ret[:, j] = affine(axes(P.basis, 1), pts[j] .. pts[j+1])[g]
-    end
-    ret
-end
+grid(V::SubQuasiArray{T,2,<:ContinuousPolynomial{0},<:Tuple{Inclusion,BlockSlice}}) where {T} =
+    grid(view(PiecewisePolynomial(parent(V)), parentindices(V)...))
 
 function grid(V::SubQuasiArray{T,2,<:ContinuousPolynomial{1},<:Tuple{Inclusion,BlockSlice}}) where {T}
     P = parent(V)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using PiecewiseSpectralMethods, ClassicalOrthogonalPolynomials, Test
 
 
+@testset "transform" begin
+    r = range(-1, 1; length=2)
+
+    r = range(-1, 1; length=4)
+    P = PiecewisePolynomial(Chebyshev(), r)
+    x = axes(P,1)
+    @test P[:,Block.(Base.OneTo(3))] \ x ≈ (P\ x)[Block.(1:3)]
+    @test (P/P\x)[0.1] ≈ 0.1
+    
+
+    r = range(-1, 1; length=10_000)
+    P = PiecewisePolynomial(Chebyshev(), r); Pₙ = P[:,Block.(1:1000)]; x = axes(P,1)
+    @time u = Pₙ / Pₙ \ cos.(10_000x.^2);
+    u[0.1] ≈ cos(10_000*0.1^2)
+
+    P = ContinuousPolynomial{0}(r)
+    x = axes(P,1)
+    P[:,Block.(Base.OneTo(3))] \ x
+end
+
 @testset "lowering" begin
     c = [randn(20); zeros(∞)]
     for r in (range(-1, 1; length=2), range(0, 1; length=2), range(0, 1; length=4))
@@ -13,14 +33,13 @@ using PiecewiseSpectralMethods, ClassicalOrthogonalPolynomials, Test
     end
 end
 
-
 @testset "mass" begin
-    r = range(-1,1; length=2)
-    r = range(0,1; length=4)
-    P = ContinuousPolynomial{0}(r)
-    C = ContinuousPolynomial{1}(r)
-    P'P
-    C'C
+    for r in (range(-1,1; length=2), range(0,1; length=4), range(0, 1; length=4))
+        P = ContinuousPolynomial{0}(r)
+        C = ContinuousPolynomial{1}(r)
+        P'P
+        C'C
+    end
 end
 
 @testset "derivative" begin
@@ -30,4 +49,12 @@ end
     x = axes(C,1)
     D = Derivative(x)
     P\D*C
+end
+
+@testset "weak Laplacian" begin
+    r = range(0,1; length=4)
+    C = ContinuousPolynomial{1}(r)
+    x = axes(C,1)
+    D = Derivative(x)
+    (D*C)'*(D*C) + C'C
 end
