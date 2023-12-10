fecc8ab9232d1533d4e22b26ce706377629ace31
Sheehan Olver <solver@mac.com>
2023-12-10 17:21:52 +0000

    ArrowheadMatrix -> BBBArrowheadMatrix (#61)

--- src/arrowhead.jl
+++ src/arrowhead.jl
@@ -1,52 +1,52 @@
 
 """
-ArrowheadMatrix
+BBBArrowheadMatrix
 
     A   B   B
     C   D   D  D
     …   …   …   …   …
     C   D   D
 """
-struct ArrowheadMatrix{T, AA<:AbstractMatrix{T}, BB, CC, DD} <: AbstractBandedBlockBandedMatrix{T}
+struct BBBArrowheadMatrix{T, AA<:AbstractMatrix{T}, BB, CC, DD} <: AbstractBandedBlockBandedMatrix{T}
     A::AA
     B::BB # first row blocks
     C::CC # first col blocks
     D::DD # these are interlaces
 
-    ArrowheadMatrix{T, AA, BB, CC, DD}(A, B, C, D) where {T,AA,BB,CC,DD} = new{T,AA,BB,CC,DD}(A, B, C, D)
+    BBBArrowheadMatrix{T, AA, BB, CC, DD}(A, B, C, D) where {T,AA,BB,CC,DD} = new{T,AA,BB,CC,DD}(A, B, C, D)
 end
 
-ArrowheadMatrix{T}(A::AbstractMatrix{T}, B, C, D) where T = ArrowheadMatrix{T, typeof(A), typeof(B), typeof(C), typeof(D)}(A, B, C, D)
-ArrowheadMatrix{T}(A, B, C, D) where T = ArrowheadMatrix(convert(AbstractMatrix{T}, A), B, C, D)
+BBBArrowheadMatrix{T}(A::AbstractMatrix{T}, B, C, D) where T = BBBArrowheadMatrix{T, typeof(A), typeof(B), typeof(C), typeof(D)}(A, B, C, D)
+BBBArrowheadMatrix{T}(A, B, C, D) where T = BBBArrowheadMatrix(convert(AbstractMatrix{T}, A), B, C, D)
 
-const ArrowheadMatrices = Union{ArrowheadMatrix,Symmetric{<:Any,<:ArrowheadMatrix},Hermitian{<:Any,<:ArrowheadMatrix},
-                                UpperOrLowerTriangular{<:Any,<:ArrowheadMatrix}}
+const ArrowheadMatrices = Union{BBBArrowheadMatrix,Symmetric{<:Any,<:BBBArrowheadMatrix},Hermitian{<:Any,<:BBBArrowheadMatrix},
+                                UpperOrLowerTriangular{<:Any,<:BBBArrowheadMatrix}}
 
                                 
 
 subblockbandwidths(A::ArrowheadMatrices) = (1,1)
 
-BlockArrays._show_typeof(io::IO, B::ArrowheadMatrix{T}) where T = print(io, "ArrowheadMatrix{$T}")
+BlockArrays._show_typeof(io::IO, B::BBBArrowheadMatrix{T}) where T = print(io, "BBBArrowheadMatrix{$T}")
 
-function blockbandwidths(A::ArrowheadMatrix)
+function blockbandwidths(A::BBBArrowheadMatrix)
     l,u = bandwidths(A.D[1])
     max(l,length(A.C)),max(u,length(A.B))
 end
 
-function axes(L::ArrowheadMatrix)
+function axes(L::BBBArrowheadMatrix)
     ξ,n = size(L.A)
     m = length(L.D)
     μ,ν = size(L.D[1])
     blockedrange(Vcat(ξ, Fill(m,μ))), blockedrange(Vcat(n, Fill(m,ν)))
 end
 
-copy(A::ArrowheadMatrix) = ArrowheadMatrix(copy(A.A), map(copy, A.B), map(copy, A.C), map(copy, A.D))
+copy(A::BBBArrowheadMatrix) = BBBArrowheadMatrix(copy(A.A), map(copy, A.B), map(copy, A.C), map(copy, A.D))
 
 for adj in (:adjoint, :transpose)
-    @eval $adj(A::ArrowheadMatrix) = ArrowheadMatrix($adj(A.A), map($adj, A.C), map($adj, A.B), map($adj, A.D))
+    @eval $adj(A::BBBArrowheadMatrix) = BBBArrowheadMatrix($adj(A.A), map($adj, A.C), map($adj, A.B), map($adj, A.D))
 end
 
-function getindex(L::ArrowheadMatrix{T}, Kk::BlockIndex{1}, Jj::BlockIndex{1})::T where T
+function getindex(L::BBBArrowheadMatrix{T}, Kk::BlockIndex{1}, Jj::BlockIndex{1})::T where T
     K,k = block(Kk),blockindex(Kk)
     J,j = block(Jj),blockindex(Jj)
     J == K == Block(1) && return L.A[k,j]
@@ -60,12 +60,12 @@ function getindex(L::ArrowheadMatrix{T}, Kk::BlockIndex{1}, Jj::BlockIndex{1})::
     return L.D[k][Int(K)-1, Int(J)-1]
 end
 
-function getindex(L::ArrowheadMatrix, k::Int, j::Int)
+function getindex(L::BBBArrowheadMatrix, k::Int, j::Int)
     ax,bx = axes(L)
     L[findblockindex(ax, k), findblockindex(bx, j)]
 end
 
-function ArrowheadMatrix(A, B, C, D)
+function BBBArrowheadMatrix(A, B, C, D)
     ξ,n = size(A)
     m = length(D)
     μ,ν = size(D[1])
@@ -95,7 +95,7 @@ function ArrowheadMatrix(A, B, C, D)
     end
     T = promote_type(eltype(A), mapreduce(eltype, promote_type, B; init=eltype(A)),
                      mapreduce(eltype, promote_type, C; init=eltype(A)), mapreduce(eltype, promote_type, D; init=eltype(A)))
-    ArrowheadMatrix{T}(A, B, C, D)
+    BBBArrowheadMatrix{T}(A, B, C, D)
 end
 
 
@@ -113,9 +113,9 @@ arrowheadlayout(::BandedLazyLayouts) = LazyArrowheadLayout()
 arrowheadlayout(::DiagonalLayout{<:AbstractLazyLayout}) = LazyArrowheadLayout()
 symmetriclayout(lay::ArrowheadLayouts) = SymmetricLayout{typeof(lay)}()
 
-MemoryLayout(::Type{<:ArrowheadMatrix{<:Any,<:Any,<:Any,<:Any,<:AbstractVector{D}}}) where D = arrowheadlayout(MemoryLayout(D))
+MemoryLayout(::Type{<:BBBArrowheadMatrix{<:Any,<:Any,<:Any,<:Any,<:AbstractVector{D}}}) where D = arrowheadlayout(MemoryLayout(D))
 # Hack since ∞ diagonal fill is only DiagonalLayout{FillLayout}
-MemoryLayout(::Type{<:ArrowheadMatrix{<:Any,<:Any,<:Any,<:Any,<:AbstractVector{<:Diagonal{<:Any,<:AbstractFill{<:Any,1,<:Tuple{OneToInf}}}}}}) = LazyArrowheadLayout()
+MemoryLayout(::Type{<:BBBArrowheadMatrix{<:Any,<:Any,<:Any,<:Any,<:AbstractVector{<:Diagonal{<:Any,<:AbstractFill{<:Any,1,<:Tuple{OneToInf}}}}}}) = LazyArrowheadLayout()
 
 sublayout(::ArrowheadLayouts,
           ::Type{<:NTuple{2,BlockSlice{<:BlockRange{1, Tuple{OneTo{Int}}}}}}) = ArrowheadLayout()
@@ -123,14 +123,14 @@ function sub_materialize(::ArrowheadLayout, V::AbstractMatrix)
     KR,JR = parentindices(V)
     P = parent(V)
     M,N =  KR.block[end],JR.block[end]
-    ArrowheadMatrix(P.A, P.B, P.C,
+    BBBArrowheadMatrix(P.A, P.B, P.C,
                     layout_getindex.(P.D, Ref(oneto(Int(M)-1)), Ref(oneto(Int(N)-1))))
 end
 
 symmetric(A) = Symmetric(A)
 symmetric(A::Union{SymTridiagonal,Symmetric,Diagonal}) = A
 
-function getproperty(F::Symmetric{<:Any,<:ArrowheadMatrix}, d::Symbol)
+function getproperty(F::Symmetric{<:Any,<:BBBArrowheadMatrix}, d::Symbol)
     P = getfield(F, :data)
     if d == :A
         return symmetric(P.A)
@@ -242,19 +242,19 @@ end
 # Cholesky
 ####
 
-function reversecholcopy(S::Symmetric{<:Any,<:ArrowheadMatrix})
+function reversecholcopy(S::Symmetric{<:Any,<:BBBArrowheadMatrix})
     T = LinearAlgebra.choltype(S)
     A = parent(S)
-    Symmetric(ArrowheadMatrix(LinearAlgebra.copymutable_oftype(A.A, T),
+    Symmetric(BBBArrowheadMatrix(LinearAlgebra.copymutable_oftype(A.A, T),
     LinearAlgebra.copymutable_oftype.(A.B, T), LinearAlgebra.copymutable_oftype.(A.C, T),
     LinearAlgebra.copymutable_oftype.(A.D, T)))
 end
 
 
 
-function MatrixFactorizations._reverse_chol!(A::ArrowheadMatrix, ::Type{UpperTriangular})
-
-    for B in A.D
+function MatrixFactorizations._reverse_chol!(A::BBBArrowheadMatrix, ::Type{UpperTriangular})
+    
+    Threads.@threads for B in A.D
         reversecholesky!(Symmetric(B))
     end
 
@@ -384,16 +384,16 @@ tupleop(op, A::Tuple, B::Tuple) = (op(first(A), first(B)), tupleop(op, tail(A), 
 
 
 for op in (:+, :-)
-    @eval $op(A::ArrowheadMatrix, B::ArrowheadMatrix) = ArrowheadMatrix($op(A.A, B.A), tupleop($op, A.B, B.B), tupleop($op, A.C, B.C), $op(A.D, B.D))
+    @eval $op(A::BBBArrowheadMatrix, B::BBBArrowheadMatrix) = BBBArrowheadMatrix($op(A.A, B.A), tupleop($op, A.B, B.B), tupleop($op, A.C, B.C), $op(A.D, B.D))
 end
--(A::ArrowheadMatrix) = ArrowheadMatrix(-A.A, map(-, A.B), map(-, A.C), -A.D)
+-(A::BBBArrowheadMatrix) = BBBArrowheadMatrix(-A.A, map(-, A.B), map(-, A.C), -A.D)
 
 for op in (:*, :\)
-    @eval $op(c::Number, A::ArrowheadMatrix) = ArrowheadMatrix($op(c, A.A), broadcast($op, c, A.B), broadcast($op, c, A.C), broadcast($op, c, A.D))
+    @eval $op(c::Number, A::BBBArrowheadMatrix) = BBBArrowheadMatrix($op(c, A.A), broadcast($op, c, A.B), broadcast($op, c, A.C), broadcast($op, c, A.D))
 end
 
 for op in (:*, :/)
-    @eval $op(A::ArrowheadMatrix, c::Number) = ArrowheadMatrix($op(A.A, c), broadcast($op, A.B, c), broadcast($op, A.C, c), broadcast($op, A.D, c))
+    @eval $op(A::BBBArrowheadMatrix, c::Number) = BBBArrowheadMatrix($op(A.A, c), broadcast($op, A.B, c), broadcast($op, A.C, c), broadcast($op, A.D, c))
 end
 
 ###
@@ -416,7 +416,7 @@ for (UNIT, Tri) in (('U',UnitUpperTriangular), ('N', UpperTriangular))
         A,B,D = P.A,P.B,P.D
         m = length(D)
 
-        for k = 1:m
+        Threads.@threads for k = 1:m
             ArrayLayouts.ldiv!($Tri(D[k]), view(dest, n+k:m:length(dest)))
         end
 
@@ -467,7 +467,7 @@ end
 
 
 for Tri in (:UpperTriangular, :UnitUpperTriangular)
-    @eval function getproperty(F::$Tri{<:Any,<:ArrowheadMatrix}, d::Symbol)
+    @eval function getproperty(F::$Tri{<:Any,<:BBBArrowheadMatrix}, d::Symbol)
         P = getfield(F, :data)
         if d == :A
             return $Tri(P.A)
@@ -484,7 +484,7 @@ for Tri in (:UpperTriangular, :UnitUpperTriangular)
 end
 
 for Tri in (:LowerTriangular, :UnitLowerTriangular)
-    @eval function getproperty(F::$Tri{<:Any,<:ArrowheadMatrix}, d::Symbol)
+    @eval function getproperty(F::$Tri{<:Any,<:BBBArrowheadMatrix}, d::Symbol)
         P = getfield(F, :data)
         if d == :A
             return $Tri(P.A)
